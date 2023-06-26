1fbe059b98f9046dfc9145ee49b8b93c6bb6f0f5
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-06-26 11:51:29 -0400

    Update page.tsx

--- food-order-ui/app/page.tsx
+++ food-order-ui/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="text-teal-500/10 text-3xl">Hello Hungry People!</div>
+    <div className="text-teal-500/10 text-6xl">Hello Hungry People!</div>
   )
 }
