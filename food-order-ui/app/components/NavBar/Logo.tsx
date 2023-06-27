71d29ead7f51643a86bc56a58f2c5e37611dd840
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-06-27 17:23:10 -0400

    Update Logo.tsx

--- food-order-ui/app/components/NavBar/Logo.tsx
+++ food-order-ui/app/components/NavBar/Logo.tsx
@@ -7,6 +7,7 @@ const Logo = () => {
     const Router = useRouter();
     return ( 
         <Image
+            onClick={() => Router.push('/')}
             alt="Logo"
             className="hidden md:block cursor-pointer"
             height="30"
