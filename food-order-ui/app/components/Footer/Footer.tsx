e85e1e93767a811148039b2ef781511f1aabb0de
srinivaspradhan64 <srinivaspradhan@gmail.com>
2023-08-13 17:03:23 -0400

    Update Footer.tsx

--- food-order-ui/app/components/Footer/Footer.tsx
+++ food-order-ui/app/components/Footer/Footer.tsx
@@ -44,7 +44,60 @@ const Footer = () => {
                 items-center
             "
             >
-                Subscribe
+                <form className="
+                    m-2 
+                    flex
+                    space-x-4
+                "
+                >
+                    <input className="
+                        rounded-full
+                        transition 
+                        ease-in-out 
+                        delay-15
+                        hover:-translate-y-1 
+                        hover:scale-110
+                        duration-300
+                        h-10 
+                        px-4
+                        m-1
+                        md:m-2
+                        border-solid
+                        border-2
+                      border-rose-950
+                        text-emerald-950 
+                        hover:text-red-800
+                        font-semibold
+                        cursor-pointer
+                        " 
+                        placeholder="your@mail.com"
+                        />  
+                        <button className="
+                            px-8 
+                            rounded-full
+                            transition 
+                            ease-in-out 
+                            delay-15
+                            hover:-translate-y-1 
+                            hover:scale-110
+                            duration-300
+                            h-10 
+                            px-4
+                            m-1
+                            md:m-2
+                            border-solid
+                            border-2
+                        border-rose-950
+                            text-emerald-950 
+                            hover:text-red-800
+                            font-semibold
+                            cursor-pointer
+                            "
+                            >
+                                Subscribe
+                            </button>
+
+                </form>
             </div>
             <div className="
                 flex
